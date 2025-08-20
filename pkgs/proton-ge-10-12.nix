538788252670fab6a5049ae595874c158fc7cf9c
KitKat <michael.williams@brighter-applications.com>
2025-08-20 21:15:13 +0100

    Fixing something

--- pkgs/proton-ge-10-12.nix
+++ pkgs/proton-ge-10-12.nix
@@ -1,30 +1,21 @@
-{ stdenv, fetchurl, writeShellScript, ... }:
+{ stdenv, fetchurl }:
 
 stdenv.mkDerivation rec {
-  pname = "proton-ge-custom";
-  version = "GE-Proton10-12";
+  pname = "GE-Proton10-12";
+  version = "10-12";
 
   src = fetchurl {
     url = "https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton10-12/GE-Proton10-12.tar.gz";
     sha256 = "47dffcff346b35c75649a95a012ad4e9b6376087dceb43ac7b695e04f5ed3c1e";
   };
 
-  dontBuild = true;
-  dontConfigure = true;
-  dontFixup = true;
-
   installPhase = ''
-    runHook preInstall
-    
-    # Create the directory structure expected by Steam
     mkdir -p $out
-    cp -r ./* $out/
-    
-    runHook postInstall
+    tar -xzf $src -C $out --strip-components=1
   '';
 
   meta = {
-    description = "Proton GE Custom 10-12 - Wine-based compatibility tool for Steam";
+    description = "Proton GE Custom 10-12";
     homepage = "https://github.com/GloriousEggroll/proton-ge-custom";
     platforms = [ "x86_64-linux" ];
   };
